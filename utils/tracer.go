25afc647fe957c0eae517f345b9fe0222577cf49
StevanoZ <stevanoseiryuu@Stevanos-MacBook-Pro-2.local>
2022-06-14 22:29:25 +0700

    feat: added tracer

--- utils/tracer.go
+++ utils/tracer.go
@@ -2,14 +2,32 @@ package shrd_utils
 
 import (
 	"context"
+	"errors"
 
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-func SetUpTracerSpan(ctx context.Context, identifier string) ddtrace.Span {
+func SetupTracer(ctx context.Context, identifier string) func() {
 	trac, errTrac := tracer.StartSpanFromContext(ctx, identifier)
 	LogIfError(errTrac.Err())
 
-	return trac
+	return func() {
+		err := recover()
+
+		if err != nil {
+			appErr, isAppErr := err.(AppError)
+			unknownErr, isUnknownErr := err.(error)
+			if isAppErr {
+				trac.Finish(tracer.WithError(errors.New(appErr.Message)))
+			}
+
+			if isUnknownErr {
+				trac.Finish(tracer.WithError(unknownErr))
+			}
+
+			panic(err)
+		} else {
+			trac.Finish(tracer.NoDebugStack())
+		}
+	}
 }
